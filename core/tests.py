8d360b7935df42902f12d30550bdfffd7987c5be
charoensinko <charoensinl@yahoo.com>
2026-03-17 20:03:03 +0700

    Django Project Test Passed

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
-
-# Create your tests here.
+from .models import Post
+class PostModelTest(TestCase):
+    def test_create_post(self):
+        post = Post.objects.create(
+            title="Test Title",
+            content="Test Content"
+        )
+        self.assertEqual(post.title, "Test Title")
