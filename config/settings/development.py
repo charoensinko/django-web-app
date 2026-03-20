51adf1a02e7215920329323dc25d1de289cea302
charoensinko <charoensinl@yahoo.com>
2026-03-20 17:57:56 +0700

    Django Project Before Neon 6

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,4 +1,21 @@
 from .base import *
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT", "5432"),
+    }
+}
