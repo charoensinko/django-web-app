b5f248a520ab06b7eda11762d06cf79902250e6c
charoensinko <charoensinl@yahoo.com>
2026-03-20 12:07:40 +0700

    Django Project Before Neon 2

--- config/asgi.py
+++ config/asgi.py
@@ -8,15 +8,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-from pathlib import Path
-
 from django.core.asgi import get_asgi_application
-from dotenv import load_dotenv
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-load_dotenv(BASE_DIR / ".env")
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,15 +8,7 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-from pathlib import Path
-
 from django.core.wsgi import get_wsgi_application
-from dotenv import load_dotenv
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-load_dotenv(BASE_DIR / ".env")
 
 os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE",
